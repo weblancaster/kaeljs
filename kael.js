904c36d81c58c32f82d21833cda4d8b7b5292f6c
Michael Lancaster <free005304@amchiFRL0005304.local>
2014-01-16 16:17:46 -0600

    add cli color and handle user input for folder name configuration

--- kael.js
+++ kael.js
@@ -1,7 +1,7 @@
 /** @license
  * kael <https://github.com/weblancaster/kael/>
  * Author: Michael Lancaster | MIT License
- * v0.0.1 (2014/01/13)
+ * v0.0.2 (2014/01/13)
  */
 
 (function() {
@@ -11,6 +11,8 @@ var path = require('path');
 var fs = require('fs');
 var exec = require('child_process').exec;
 var marked = require('marked');
+var clc = require('cli-color');
+var readline = require('readline');
 
 // set marked module options
 marked.setOptions({
@@ -24,9 +26,74 @@ marked.setOptions({
     smartypants: true
 });
 
+// default color messages
+var logError = clc.red.bold;
+var logWarn = clc.yellow;
+var logNotice = clc.blue;
+var logSuccess = clc.green;
+
+// set readline for CLI
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// default messages
+var messages = {
+    initial: 'Initializing process...',
+    markdownIn: 'Please enter the name of the folder where the markdown files are saved. If empty or answered as no it will use default (markdown).',
+    htmlIn: 'Please enter the name of the folder where the HTML will be generated. If empty or answered as no it will use default (blog).'
+}
+
 // Global var's defined
-var markdownFolder = 'markdown/';
-var blogFolder = 'blog/';
+var markdownFolder;
+var blogFolder;
+
+/**
+ * Responsible to config the markdown folder
+ * @method configHTMLFolder
+ */
+function configMarkdownFolder() {
+    // Markdown default message
+    console.log( logWarn(messages.markdownIn) );
+
+    rl.question('Do you want to set a new folder for your markdown files? (folder name or no): ', function (answer) {
+        console.log('input ' + typeof answer + answer);
+
+        if ( answer === 'no' || answer === '' ) {
+            markdownFolder = 'markdown/';
+            console.log( logSuccess('Default folder markdown saved successfully') );
+        } else {
+            markdownFolder = answer + '/';
+            console.log( logSuccess('Folder ' + answer +' saved successfully') );
+        }
+
+        // config blog folder
+        configHTMLFolder();
+    });
+}
+
+/**
+ * Responsible to config the blog folder
+ * @method configHTMLFolder
+ */
+function configHTMLFolder() {
+    // HTML default message
+    console.log( logWarn(messages.htmlIn) );
+
+    rl.question('Do you want to set a new folder for your generated HTML files? (folder name or no): ', function (answer) {
+        if ( answer === 'no' || answer === '' ) {
+            blogFolder = 'blog/';
+            console.log( logSuccess('default folder blog saved successfully') );
+        } else {
+            blogFolder = answer + '/';
+            console.log( logSuccess('Folder ' + answer +' saved successfully') );
+        }
+
+        // start build markdowns
+        readMarkdown();
+    });
+}
 
 /**
  * Responsible to go through all markdown files
@@ -34,12 +101,11 @@ var blogFolder = 'blog/';
  * also checks if there's a blog folder with the same name already
  * @method readMarkdown
  */
-
 function readMarkdown() {
 
     fs.readdir(markdownFolder, function(error, data) {
         if ( error ) {
-            console.log('Folder "markdown" not found');
+            console.log( logError('Folder which contains the markdown files not found') );
             return false;
         }
 
@@ -52,6 +118,7 @@ function readMarkdown() {
             createHTML(file, fileName);
         }
 
+        rl.close();
     });
 }
 
@@ -66,7 +133,7 @@ function createFolder(folder, file) {
 
     if ( !fs.existsSync(folder) ) {
         fs.mkdirSync(folder);
-        console.log('Folder: ' + folder + ' created.');
+        console.log( logWarn('Folder: ' + folder + ' created.') );
     } else {
         removeFolder(folder);
     }
@@ -80,7 +147,7 @@ function createFolder(folder, file) {
 function removeFolder(folder) {
     var command = 'rm -rf ' + folder;
     exec(command, function(error, out) {
-        console.log('Folder: ' + folder + ' removed');
+        console.log( logWarn('Folder: ' + folder + ' removed') );
     });
 }
 
@@ -98,15 +165,27 @@ function createHTML(file, fileName) {
 
         fs.writeFile(htmlPath, html, function (error) {
             if ( error ) {
-                console.log('Error converting markdown to HTML: ' + error);
+                console.log( logError('Error converting markdown to HTML: ' + error) );
             }
-            console.log('File: ' + file + ' converted to HTML');
+            console.log( logSuccess('File: ' + file + ' converted to HTML') );
         });
 
     });
 }
 
+/**
+ * Responsible to start up the build process
+ * @method startUp
+ */
+function startUp() {
+    // Initial message
+    console.log( logSuccess(messages.initial) );
+
+    // config markdown folder
+    configMarkdownFolder();
+}
+
 // Initialize build process
-readMarkdown();
+startUp();
 
 })();
