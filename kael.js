9980784e66c5e0581225a39771d11e968adb3d80
Michael Lancaster <free005304@amchiFRL0005304.local>
2014-02-13 16:26:48 -0600

    add changes related to the new way it will build the blog pages

---
+++ kael.js
@@ -0,0 +1,28 @@
+var kael = require('./lib/kael')
+    , watch = require('watch')
+    , moment = require('moment');
+
+(function(){
+
+    // markdown path folder
+    var mdFiles = './markdown/';
+
+    watch.watchTree(mdFiles, function (f, curr, prev) {
+        var dateNow = moment().format('MMM Do YY, h:mm:ss a');
+
+        if (typeof f == "object" && prev === null && curr === null) {
+          console.log(dateNow, 'Watch markdown files running!');
+          kael();
+        } else if (prev === null) {
+          console.log(dateNow, 'New markdown file added');
+          kael();
+        } else if (curr.nlink === 0) {
+          console.log(dateNow, 'Markdown file removed');
+          kael();
+        } else {
+          console.log(dateNow, 'Markdown file changed');
+          kael();
+        }
+    });
+
+})();
